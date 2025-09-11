bd76f3ef7f674fbde85a064cd96ecf2e1bfc9257
Inma Gijón <igijoninf@gmail.com>
2025-09-11 20:03:56 +0200

    Template String

---
+++ src/bases/02-template-string.ts
@@ -0,0 +1,12 @@
+
+const firstName = 'Inma';
+// const lastName = "O'Neal \" entre comillas \"";
+const lastName = `O'Neal`;
+
+//Template string
+// const fullName = `
+// El nombre es:
+//     ${firstName} ${lastName}
+// `;
+const fullName = `${firstName} ${lastName} ${2+4}`;
+console.log(fullName);
